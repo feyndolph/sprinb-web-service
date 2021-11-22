a8c198b3f5ecb6fb4f0df8b7fafc2d9971c4ed3d
KIMDOKYUNG <>
2021-11-22 12:16:20 +0900

    프로젝트 생성

---
+++ src/main/java/com/springweb/springwebservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.springweb.springwebservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello World";
+    }
+}
